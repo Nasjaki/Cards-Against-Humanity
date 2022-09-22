206b7f7acbb5c93507a6ca53bb96627f520e6feb
Nasjaki <Niklasragnar.stoll@student.hs-anhalt.de>
2022-09-22 15:55:26 +0200

    Nice Fix

--- src/component/errorpage.js
+++ src/component/errorpage.js
@@ -1,8 +1,24 @@
 
 import React from 'react';
 
+import { useState, useEffect } from "react";
+
+import {
+    useNavigate
+  } from 'react-router-dom';
 
 export default function ErrorPage (){
+    const navigate = useNavigate();
+
+    //mount
+    useEffect(() => {
+        setTimeout(async () => {
+            navigate("/");
+        }, []); 
+    });
+
+    
+
     return <div>
         <h1>Error</h1>
 
