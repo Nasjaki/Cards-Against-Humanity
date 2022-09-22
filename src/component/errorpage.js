95ea578bca1ba80aa98141fd0c6a63d229cd039e
Nasjaki <Niklasragnar.stoll@student.hs-anhalt.de>
2022-09-22 17:53:53 +0200

    revert errorpage

--- src/component/errorpage.js
+++ src/component/errorpage.js
@@ -1,23 +1,8 @@
 
 import React from 'react';
 
-import { useState, useEffect } from "react";
-
-import {
-    useNavigate
-  } from 'react-router-dom';
 
 export default function ErrorPage (){
-    const navigate = useNavigate();
-
-    //mount
-    useEffect(() => {
-        setTimeout(async () => {
-            navigate("/");
-        }, []); 
-    });
-
-    
 
     return <div>
         <h1>Error</h1>
